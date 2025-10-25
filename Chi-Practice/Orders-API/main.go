60b1bd8d309398e7093abd35a689ace4c72685d8
Sunil Maharana <maharanasunil38@gmail.com>
2025-10-25 20:01:10 +0530

    chi router and middleware added

--- Chi-Practice/Orders-API/main.go
+++ Chi-Practice/Orders-API/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
 
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
+
+	// Instantiate a http server
+	server := &http.Server{
+		Addr:    ":3000", // Port
+		Handler: router,  // in chi the router type itself confirms to the http handler
+		// http.HandlerFunc(basicHandler), // this function will be called when our server receives a request
+	}
+
+	// Call our server
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen and serve", err)
+		return
+	}
+}
+
+func basicHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Hello, Sunil!"))
 }
