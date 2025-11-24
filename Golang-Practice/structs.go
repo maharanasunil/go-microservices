703e0133cb34f9cfd916a1824be6087a3cad7183
maharanasunilgrowtharc <sunil.maharana@growtharc.com>
2025-11-24 12:10:41 +0530

    structs practice

--- Golang-Practice/structs.go
+++ Golang-Practice/structs.go
@@ -12,6 +12,16 @@ type user struct {
 	createdAt time.Time
 }
 
+// This function belongs to the strict user
+func (u user) printUserData() {
+	fmt.Println(u.firstname, u.lastname, u.dob, u.createdAt)
+}
+
+func (u *user) clearUserName() {
+	u.firstname = ""
+	u.lastname = ""
+}
+
 func main() {
 	userFirstname := get_user_data("Enter your First Name: ")
 	userLastname := get_user_data("Enter your Last Name: ")
@@ -27,10 +37,14 @@ func main() {
 		createdAt: time.Now(),
 	}
 
-	//.. Do something with that data
 	// fmt.Println(userFirstname, userLastname, userDob)
-	outputUserDetails(&appUser) // We are passing address of our struct object
+	// outputUserDetails(&appUser) // We are passing address of our struct object
 	// Else a new copy of struct is created in memory when this func is called
+
+	// We added a new method to our struct
+	appUser.printUserData()
+	appUser.clearUserName()
+	appUser.printUserData()
 
 }
 
